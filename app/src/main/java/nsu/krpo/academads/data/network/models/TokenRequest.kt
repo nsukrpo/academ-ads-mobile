bee7e7d829db646b9b3be1044c45556e2748728d
animesosok <danilreutsky@gmail.com>
2023-12-05 14:32:35 +0300

    changes

--- app/src/main/java/nsu/krpo/academads/data/network/models/TokenRequest.kt
+++ app/src/main/java/nsu/krpo/academads/data/network/models/TokenRequest.kt
@@ -3,9 +3,6 @@ package nsu.krpo.academads.data.network.models
 import com.squareup.moshi.Json
 
 data class TokenRequest(
-        @Json(name = "user_id")
-        val userId: Long,
-
         @Json(name = "login")
         val login: String,
 
